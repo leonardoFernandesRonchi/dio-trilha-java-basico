d3419b531c5b58722853272981f1a6b14c2d8136
Leonardo Ronchi <leofronchi@outlook.com>
2024-05-13 09:53:00 -0300

    Mudanças totais

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+     Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+     System.out.println("Por favor, digite o número da conta");
+     int AccountNumber = scanner.nextInt();
+     System.out.println("Por favor, Digite o número da Agência");
+     String AgencyNumber = scanner.next();
+     System.out.println("Por favor, Digite o nome do cliente");
+     String ClientName = scanner.next();
+     System.out.println("Por favor, Digite o saldo");
+     float Balance = scanner.nextFloat();
+
+
+
+     System.out.println("Olá," + ClientName + ", obrigado por criar uma conta em nosso banco, sua agência é" + AgencyNumber + ", conta" + AccountNumber + "e seu saldo" + Balance + "Já está disponível para saque.");
     }
+
 }
